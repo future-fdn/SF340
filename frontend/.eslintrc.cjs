6676c83728476e8a46863d96e650bcecc4d76f47
Tinn-K <tinn.kanjananuwat@gmail.com>
2024-05-02 09:37:39 +0700

    fix bugs

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -21,7 +21,11 @@ const config = {
     "react/jsx-key": "off",
     "drizzle/enforce-delete-with-where": "off",
     "@typescript-eslint/no-empty-interface": "off",
-
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
